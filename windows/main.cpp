ebc696b0f7526f8afad6b4c2a1fe83b216679c21
Kaktuszon <kaktus@kaktuszon.org>
2023-07-08 13:04:39 +0200

    Initial commit

---
+++ windows/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <windows.h>
+
+void leftClick();
+
+int main() {
+    float mouse_x; //X position of the mouse
+    float mouse_y; //Y position of the mouse
+    float mouse_speed = 0.005;
+
+    POINT pointer; //Mouse pointer
+
+    //Set the X & Y position from the mouse
+    if(GetCursorPos(&pointer)) {
+        mouse_x = pointer.x;
+        mouse_y = pointer.y;
+    }
+
+    while(!GetAsyncKeyState(VK_END)) {
+        if((GetKeyState(VK_CAPITAL) & 0x001) != 0) {
+            std::cout << "Caps is pressed!" << std::endl;
+        }
+
+        if(GetAsyncKeyState(VK_LEFT)) {
+            mouse_x = mouse_x - mouse_speed;
+            SetCursorPos(mouse_x, mouse_y);
+        }
+
+        if(GetAsyncKeyState(VK_RIGHT)) {
+            mouse_x = mouse_x + mouse_speed;
+            SetCursorPos(mouse_x, mouse_y);
+        }
+
+        if(GetAsyncKeyState(VK_UP)) {
+            mouse_y = mouse_y - mouse_speed;
+            SetCursorPos(mouse_x, mouse_y);
+        }
+
+        if(GetAsyncKeyState(VK_DOWN)) {
+            mouse_y = mouse_y + mouse_speed;
+            SetCursorPos(mouse_x, mouse_y);
+        }
+
+        if(GetAsyncKeyState(VK_SPACE)) {
+            leftClick();
+        }
+    }
+
+    return 0;
+}
+
+void leftClick() {
+    INPUT input[2] = {};
+    input[0].type = INPUT_MOUSE;
+    input[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+
+    input[1].type = INPUT_MOUSE;
+    input[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+    Sleep(100); //So we cannot spam by holding the button
+
+    SendInput(ARRAYSIZE(input), input, sizeof(INPUT));
+}
